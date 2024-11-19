751ddc70e5f40f41d02b83b6c6f6fb87dad0c801
imbkp010 <BRYANKYLE.PARAON@live.ama.edu.ph>
2024-11-19 07:14:28 +0000

    Deploying to gh-pages from @ imbkp010/ama-university-chatbotV8@e1d8e6603fb0ac274c5a4954c856970078a1d7f5 🚀

--- public/scripts/knowledge.js
+++ public/scripts/knowledge.js
@@ -5,6 +5,8 @@ Career Profiler: https://www.amaes.edu.ph/career-profiler/
 About AMA: https://www.amaes.edu.ph/about-the-ama-education-system/
 
 Programs:
+- Computer Engineering: https://www.amaes.edu.ph/programs-offered-engineering/
+- Computer Studies: https://www.amaes.edu.ph/programs-offered-computing/
 - Basic Education: https://www.amaes.edu.ph/programs-offered-basic-education/
 - Aviation/Delta Air: https://www.amaes.edu.ph/programs-offered-aviation-delta-air/
 - Medical/Nursing/Allied Health: https://www.amaes.edu.ph/programs-offered-medical-nursing-allied-health/
@@ -13,8 +15,6 @@ Programs:
 - Education: https://www.amaes.edu.ph/programs-offered-education/
 - Hospitality and Tourism: https://www.amaes.edu.ph/programs-offered-hospitality-tourism/
 - Business Administration: https://www.amaes.edu.ph/programs-offered-business-and-management/
-- Computer Engineering: https://www.amaes.edu.ph/programs-offered-engineering/
-- Computer Studies: https://www.amaes.edu.ph/programs-offered-computing/
 
 Student Services:
 - Enrollment Schedule: https://www.amaes.edu.ph/enrollment-schedule-for-ama-senior-high/
@@ -29,6 +29,24 @@ AMA University Main Campus
 - Adress:  Maximina St., Villa Arca Subdivision, Project 8, Quezon City
 - Hotline: (02) 8656-0654 / 88443225
 - Mobile:  0995 721 1749
+- Email: info@amaes.edu.ph
+
+About the AMA Education System
+- The AMA Education System is a leading provider of IT-based education in the Philippines, and its flagship institution, AMA University and Colleges, is a member of this esteemed system. Founded by Dr. Amable R. Aguiluz V in 1980 and named after his father, Hon. Amable M. Aguiluz, AMA has become the largest network of colleges and universities in Asia, producing graduates who are highly competent in various technologies and applications needed in the industry.
+- AMA's focus on IT education means that technology is integrated into all of its programs, providing students with an advantage in the use of different industry-required business applications and making them tech-savvy. The AMA Education System is dedicated to providing quality education that prepares students for promising careers, and as the pioneer in IT education and online learning in the Philippines, AMAES offers a holistic, high-quality education to produce graduates who are ready to meet the demands of the times. This commitment to excellence has made AMA Education System a reliable and trusted provider of education in the Philippines.
+
+5 Reasons Why AMA University Remain to be a Top Choice for Excellent IT Education:
+1. A Pioneer in the field of IT Education.
+-AMA University was founded in 1980 by Dr. Amable R. Aguiluz V who saw the demand for professionally trained computer professionals in the country that time. Focusing mainly on computer related programs, AMA was indeed an institution that falls under the classification of “computer school” even back then. Currently, AMA has extended its field of expertise by offering other programs, but nevertheless, all programs include subjects that would help its students become proficient in technology.
+2. Quality Graduates.
+-A recent article published by entrepreneur.com.ph states that based on the data shared by Jobstreet.com, graduates of AMA are among the highest paid employees in the country. This makes sense, especially if we consider the skills required by the industry today and what most AMA graduates would probably possess, which is of course, IT skills. Almost every company today will have significant requirement for people with knowledge in technology, which makes AMA graduates fit for the need.
+3. Industry Partners.
+-AMA chooses its partners well. It knows what brands or companies would best match its reputation and will help improve the quality of education it delivers. AMA usually tie-up with tech companies that would be willing to share their technology to the institution for the students to use. As a result, the students get to learn how to operate different applications and technology, while the partner creates users of their product and generate potential customers as well.
+4. Advanced Learning Method.
+-Eversince it started operating, it has been ahead in terms of delivering lessons using technology. Recently, AMA launched its newest program called AMA Online Education. It also implements blended learning method for its Senior High School program, wherein students experience blended learning, which combines face to face and online classes.
+5. Highly Accredited.
+-AMA is accredited by reputable institutions and regulating bodies for programs aligned to its areas of expertise. Most of its accreditation were given or awarded because of its excellent standing and performance in the fields where it specializes, and for meeting the standards set by the accrediting bodies. Accreditation is important, as it is a way of ensuring that the quality of education that an institution will deliver is excellent, and its students will be highly competent when they graduate.
+
 
 Requirements for New Students:
 - For a seamless enrollment, we kindly ask for these documents:
