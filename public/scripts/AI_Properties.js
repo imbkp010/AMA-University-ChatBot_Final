893ba17073abb0800ddeb180e90c9dc63baec9c7
imbkp010 <BRYANKYLE.PARAON@live.ama.edu.ph>
2024-11-24 08:26:18 +0000

    Deploying to gh-pages from @ imbkp010/Group-6_AMA-Education-System-Chatbot@aeb3a6eb5617acdc4aeb5c5470cd64410c7f95e0 🚀

---
+++ public/scripts/AI_Properties.js
@@ -0,0 +1,4 @@
+const AI_Properties = {
+    API_URL: "https://api.groq.com/openai/v1/chat/completions",
+    AI_Model: "llama-3.2-90b-text-preview"
+}
--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,1 +1,1 @@
-const config = { GROQ_API_KEY: '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' };
+const config = { API_KEY: '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' };
